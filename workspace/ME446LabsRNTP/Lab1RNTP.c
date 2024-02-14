bee578f838979b54efb92ddd3f86157acb4546c8
Rohan Nagabhirava <49493850+RohanNaga@users.noreply.github.com>
2024-02-13 23:03:01 -0600

    Update Lab1RNTP.c

--- workspace/ME446LabsRNTP/Lab1RNTP.c
+++ workspace/ME446LabsRNTP/Lab1RNTP.c
@@ -7,8 +7,8 @@
 #define GRAV        9.81
 
 // These two offsets are only used in the main file user_CRSRobot.c  You just need to create them here and find the correct offset and then these offset will adjust the encoder readings
-float offset_Enc2_rad = -0.438; //-0.37;
-float offset_Enc3_rad = 0.225; //0.27;
+float offset_Enc2_rad = -0.438; //-0.37; // offset calculated by taking read values at zero position
+float offset_Enc3_rad = 0.225; //0.27;  // offset calculated by taking read values at zero position
 
 
 // Your global varialbes.
@@ -30,32 +30,30 @@ float custom = 0.0;
 long arrayindex = 0;
 int UARTprint = 0;
 
-float printtheta1motor = 0;
-float printtheta2motor = 0;
-float printtheta3motor = 0;
+float printtheta1motor = 0;  // variable used to output theta1motor value
+float printtheta2motor = 0;  // variable used to output theta2motor value
+float printtheta3motor = 0;  // variable used to output theta3motor value
 
 // Assign these float to the values you would like to plot in Simulink
-float Simulink_PlotVar1 = 0;
+float Simulink_PlotVar1 = 0; 
 float Simulink_PlotVar2 = 0;
 float Simulink_PlotVar3 = 0;
 float Simulink_PlotVar4 = 0;
-float len = .254;
-float x = 0;
-float y = 0;
-float theta2 = 0;
-float theta3 = 0;
-float r = 0;
-float z = 0;
-float desmotortheta1 = 0;
-float desmotortheta2 = 0;
-float desmotortheta3 = 0;
-float thetag = 0;
-float h = 0;
-float q3 = 0;
-float q2 = 0;
-float thetax = 0;
-float thetay = 0;
-float d1 = .254;
+float len = .254;  // length of each of the links
+float x = 0;	   // x value of end-effector
+float y = 0;	   // y value of end-effector
+float theta2 = 0;  // theta 2 value in DH parameter values
+float theta3 = 0;  // theta 3 value in DH parameter values
+float r = 0;	   // r value computing end effector distance in the x0-y0 plane away from the x0-y0 origin
+float z = 0;	   // z value of end effector from z0 origin
+float desmotortheta1 = 0;	// calculated motor theta 1 from inverse kinematics
+float desmotortheta2 = 0;	// calculated motor theta 2 from inverse kinematics
+float desmotortheta3 = 0;	// calculated motor theta 3 from inverse kinematics
+float thetag = 0;		// intermediate theta calculation to calculate theta 3
+float h = 0;			// hypotenuse of the triangle connecting end effector position, origin, and r value. Used to calculate theta1 and theta2
+float thetax = 0;		// intermediate angle calculation for theta 2
+float thetay = 0;		// intermediate angle calculation for theta 2
+float d1 = .254;		// value for d1 in DH table
 
 void mains_code(void);
 
@@ -74,9 +72,9 @@ void main(void)
 void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float *tau2,float *tau3, int error) {
 
 
-    *tau1 = 0;
-    *tau2 = 0;
-    *tau3 = 0;
+    *tau1 = 0;		// torque value for motor 1
+    *tau2 = 0;		// torque value for motor 2
+    *tau3 = 0;		// torque value for motor 3
 
     //Motor torque limitation(Max: 5 Min: -5)
 
@@ -99,32 +97,31 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
         GpioDataRegs.GPBTOGGLE.bit.GPIO60 = 1; // Blink LED on Emergency Stop Box
     }
 
-    printtheta1motor = theta1motor;
-    printtheta2motor = theta2motor;
-    printtheta3motor = theta3motor;
+    printtheta1motor = theta1motor;		// utilized to print theta 1 value
+    printtheta2motor = theta2motor;		// utilized to print theta 2 value
+    printtheta3motor = theta3motor;		// utilized to print theta 3 value
 
-    Simulink_PlotVar1 = theta1motor;
-    Simulink_PlotVar2 = theta2motor;
-    Simulink_PlotVar3 = theta3motor;
+    Simulink_PlotVar1 = theta1motor;		// sending theta 1 value to Simulink
+    Simulink_PlotVar2 = theta2motor;		// sending theta 2 value to Simulink
+    Simulink_PlotVar3 = theta3motor;		// sending theta 3 value to Simulink
     Simulink_PlotVar4 = 0;
 
-    x = (127.0*cos(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;
-    y = (127.0*sin(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;
-    z = (127.0*cos(theta2motor))/500 - (127*sin(theta3motor))/500 + 127.0/500.0;
+	// Forward Kinematic Equations
+    x = (127.0*cos(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;    // Forward kinematic equation for end-effector x-coordinate
+    y = (127.0*sin(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;	 // Forward kinematic equation for end-effector y-coordinate
+    z = (127.0*cos(theta2motor))/500 - (127*sin(theta3motor))/500 + 127.0/500.0; // Forward kinematic equation for end-effector z-coordinate
 
-
-    r = sqrt(x*x+y*y);
-    h = sqrt(r*r+(z-d1)*(z-d1));
-    desmotortheta1 = atan2(y, x);
-    thetax = atan2(z-d1,r);
-    thetay = acos((h*h+len*len-len*len)/(2*h*len));
-    q2 = thetax+thetay;
-    desmotortheta2 = -q2 + PI/2;
-    thetag = acos((-(h*h)+len*len+len*len)/(2*len*len));
-    q3 = PI - thetag;
-    desmotortheta3 = q3 + desmotortheta2 -PI/2;
-
-
+	// Inverse Kinematic Equations
+    r = sqrt(x*x+y*y);				// r value taking the distance of the end-effector position from the origin to its position on the x0-y0 plane
+    h = sqrt(r*r+(z-d1)*(z-d1));		// hypotenuse of the triangle connecting end effector position, origin, and r value. Used to calculate theta1 and theta2
+    desmotortheta1 = atan2(y, x);		// motor 1 theta value from inverse kinematics. Theta1 in DH parameters and motor theta 1 are equal.
+    thetax = atan2(z-d1,r);			// portion of theta 2 value calculated
+    thetay = acos((h*h+len*len-len*len)/(2*h*len));	// second portion of theta 2 value calculated
+    theta2 = thetax+thetay;				// Theta 2 calculated by adding subsidiary theta values
+    desmotortheta2 = -theta2 + PI/2;			// motor theta 2 calculate
+    thetag = acos((-(h*h)+len*len+len*len)/(2*len*len));// complement of theta 3 angle used to calculate theta 3
+    theta3 = PI - thetag;				// theta 3 value calculated from thetag
+    desmotortheta3 = theta3 + desmotortheta2 -PI/2;	// Motor theta 3 calculated from theta 3
     mycount++;
 }
 
